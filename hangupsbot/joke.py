97a5a7465c868cf96cdd1cc6f277d82dd4d4df87
Srijay Kasturi (TechFilmer) <srijay@techfilmer.com>
2016-01-19 18:39:05 -0500

    make this sane
    you might wanna install the requests library but it'll be <3

--- hangupsbot/joke.py
+++ hangupsbot/joke.py
@@ -1,23 +1,12 @@
-from urllib.request import urlopen
+from requests import get
 import plugins
 
 #def _initialize():
     #plugins.register_user_command(['joke'])
 
 def joke(bot, event, *args):
-    request = urlopen('http://tambal.azurewebsites.net/joke/random')
-    json = request.read()
-    jsonlist = list(json)
-    jsonlist = [chr(x) for x in jsonlist]
-    json = ''.join(jsonlist)
-    attr = json.split(':')
-    unix = attr[1]
-    unix = unix.strip('"')
-    unixlist = list(unix)
-    unixlist[-1] = ''
-    unixlist[-2] = ''
-    unix = ''.join(unixlist)
-    joke = unix
+    x = get('http://tambal.azurewebsites.net/joke/random').json()
+    joke = x["joke"]
     if len(args) == 0:
         msg = _('{}').format(joke)
     else:
