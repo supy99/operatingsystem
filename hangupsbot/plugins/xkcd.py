9e686ac42cb9ed9dd824301187ee97b7c71f66da
2019okulkarn <2019okulkarn@tjhsst.edu>
2016-01-21 12:55:35 -0500

    added argument support for xkcd

--- hangupsbot/plugins/xkcd.py
+++ hangupsbot/plugins/xkcd.py
@@ -1,7 +1,3 @@
-"""
-Identify images, upload them to google plus, post in hangouts
-"""
-
 import aiohttp, asyncio, io, logging, os, re
 
 import plugins
@@ -22,9 +18,17 @@ def _initialise():
 def xkcd(bot, event, *args):
     # Don't handle events caused by the bot himself
     numlist =  list(range(1, 1631))
-    if args:
-        msg = _("Please don't enter any arguments")
-        yield from bot.coro_send_message(event.conv, msg)
+    if len(args) == 1 and args[0].isdigit():
+        num = int(args[0])
+        link_image = str(pykcd.XKCDStrip(num).image_link)
+        filename = os.path.basename(link_image)
+        r = yield from aiohttp.request('get', link_image)
+        raw = yield from r.read()
+        image_data = io.BytesIO(raw)
+        link = shorten(link_image)
+        image_id = yield from bot._client.upload_image(image_data, filename=filename)
+        yield from bot.coro_send_message(event.conv.id_, None, image_id=image_id)
+        yield from bot.coro_send_message(event.conv, _('{}').format(link))
     else:
         chosencomic = choose(numlist)
         chosennum = int(chosencomic)
