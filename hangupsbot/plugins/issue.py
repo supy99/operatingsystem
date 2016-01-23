9d49a56ea5469851aa0006b08a4e2eea3d0a0f16
2019okulkarn <2019okulkarn@tjhsst.edu>
2016-01-22 19:39:50 -0500

    Fixes #19 and works on fixing 13

--- hangupsbot/plugins/issue.py
+++ hangupsbot/plugins/issue.py
@@ -9,32 +9,37 @@ def _initialise():
 
 def issue(bot, event, *args):
     '''Create an issue on github.com using the given parameters.'''
-    if args:
-        url = 'https://api.github.com/repos/{}/{}/issues'.format(REPO_OWNER, REPO_NAME)
-        get = requests.get(url)
-        data = json.loads(get.text)
-        if str(args[0]).isdigit():
-            try:
-                num = int(args[0]) * -1
-                link = shorten(str(data[num][u'html_url']))
-                title = str(data[num][u'title'])
-                msg = _('{} -- {}').format(title, link)
-            except:
-                msg = _('Invalid Issue Number')
-        else:
-            session = requests.Session()
-            session.auth=(USERNAME, PASSWORD)
-            # Create our issue
-            issue = {'title': ' '.join(args),
-                     'body': 'Issue created by {}'.format(event.user.full_name)}
-            # Add the issue to our repository
-            r = session.post(url, json.dumps(issue))
-            link = shorten(str(data[0][u'html_url']))
-            if r.status_code == 201:
-                msg = _('Successfully created issue: {}').format(link)
+    try:
+        if args:
+            url = 'https://api.github.com/repos/{}/{}/issues'.format(REPO_OWNER, REPO_NAME)
+            
+            if str(args[0]).isdigit():
+                try:
+                    num = int(args[0]) * -1
+                    link = shorten(str(data[num][u'html_url']))
+                    title = str(data[num][u'title'])
+                    msg = _('{} -- {}').format(title, link)
+                except:
+                    msg = _('Invalid Issue Number')
             else:
-                msg = _('Could not create issue.<br>Response: {}').format(r.content)
+                session = requests.Session()
+                session.auth=(USERNAME, PASSWORD)
+                # Create our issue
+                issue = {'title': ' '.join(args),
+                         'body': 'Issue created by {}'.format(event.user.full_name)}
+                # Add the issue to our repository
+                get = requests.get(url)
+                data = json.loads(get.text)
+                r = session.post(url, json.dumps(issue))
+                link = shorten(str(data[0][u'html_url']))
+                if r.status_code == 201:
+                    msg = _('Successfully created issue: {}').format(link)
+                else:
+                    msg = _('Could not create issue.<br>Response: {}').format(r.content)
 
-    else:
-        msg = _('No issue given.')
-    yield from bot.coro_send_message(event.conv, msg)
+        else:
+            msg = _('No issue given.')
+        yield from bot.coro_send_message(event.conv, msg)
+    except BaseException as e:
+        msg = _('{} -- {}').format(str(e), event.text)
+        yield from bot.coro_send_message('UgwEsRHkk27NK2sRISx4AaABAQ', msg)
--- hangupsbot/plugins/xkcd.py
+++ hangupsbot/plugins/xkcd.py
@@ -17,21 +17,25 @@ def _initialise():
 
 def xkcd(bot, event, *args):
     # Don't handle events caused by the bot himself
-    numlist =  list(range(1, 1631))
-    if len(args) == 1 and args[0].isdigit():
-        num = int(args[0])
-        link_image = str(pykcd.XKCDStrip(num).image_link)
-    else:
-        chosencomic = choose(numlist)
-        chosennum = int(chosencomic)
-        link_image = str(pykcd.XKCDStrip(chosennum).image_link)
+    try:
+        numlist =  list(range(1, 1631))
+        if len(args) == 1 and args[0].isdigit():
+            num = int(args[0])
+            link_image = str(pykcd.XKCDStrip(num).image_link)
+        else:
+            chosencomic = choose(numlist)
+            chosennum = int(chosencomic)
+            link_image = str(pykcd.XKCDStrip(chosennum).image_link)
 
-    logger.info("getting {}".format(link_image))
-    filename = os.path.basename(link_image)
-    r = yield from aiohttp.request('get', link_image)
-    raw = yield from r.read()
-    image_data = io.BytesIO(raw)
-    link = shorten(link_image)
-    image_id = yield from bot._client.upload_image(image_data, filename=filename)
-    yield from bot.coro_send_message(event.conv.id_, None, image_id=image_id)
-    yield from bot.coro_send_message(event.conv, _('{}').format(link))
+        logger.info("getting {}".format(link_image))
+        filename = os.path.basename(link_image)
+        r = yield from aiohttp.request('get', link_image)
+        raw = yield from r.read()
+        image_data = io.BytesIO(raw)
+        link = shorten(link_image)
+        image_id = yield from bot._client.upload_image(image_data, filename=filename)
+        yield from bot.coro_send_message(event.conv.id_, None, image_id=image_id)
+        yield from bot.coro_send_message(event.conv, _('{}').format(link))
+    except BaseException as e:
+        msg = _('{}').format(str(e))
+        yield from bot.coro_send_message('UgwEsRHkk27NK2sRISx4AaABAQ',msg)
