10195b0483a4e00ba231c8f9f3d2a85fd2fa452e
2019okulkarn <2019okulkarn@tjhsst.edu>
2016-01-23 18:48:25 -0500

    Better URL Parsing. Fixes #33

--- hangupsbot/plugins/image_links.py
+++ hangupsbot/plugins/image_links.py
@@ -5,7 +5,7 @@ Identify images, upload them to google plus, post in hangouts
 import aiohttp, asyncio, io, logging, os, re
 
 import plugins
-
+from control import *
 from links import *
 
 logger = logging.getLogger(__name__)
@@ -17,61 +17,67 @@ def _initialise():
 
 @asyncio.coroutine
 def _watch_image_link(bot, event, command):
-    # Don't handle events caused by the bot himself
-    if event.user.is_self:
-        return
+    try:
+        # Don't handle events caused by the bot himself
+        if event.user.is_self:
+            return
 
-    if " " in event.text:
-        """immediately reject anything with spaces, must be a link"""
-        return
+        probable_image_link = False
+        event_text_lower = event.text.lower()
+        not_image_but_link = False
+        if re.match("^(https?://)?([a-z0-9.]*?\.)?imgur.com/", event_text_lower, re.IGNORECASE):
+            """imgur links can be supplied with/without protocol and extension"""
+            probable_image_link = True
 
-    probable_image_link = False
-    event_text_lower = event.text.lower()
+        elif event_text_lower.startswith(("http://", "https://")) and event_text_lower.endswith((".png", ".gif", ".gifv", ".jpg")):
+            """other image links must have protocol and end with valid extension"""
+            probable_image_link = True
 
-    if re.match("^(https?://)?([a-z0-9.]*?\.)?imgur.com/", event_text_lower, re.IGNORECASE):
-        """imgur links can be supplied with/without protocol and extension"""
-        probable_image_link = True
+        if probable_image_link and "googleusercontent" in event_text_lower:
+            """reject links posted by google to prevent endless attachment loop"""
+            logger.debug("rejected link {} with googleusercontent".format(event.text))
+            return
 
-    elif event_text_lower.startswith(("http://", "https://")) and event_text_lower.endswith((".png", ".gif", ".gifv", ".jpg")):
-        """other image links must have protocol and end with valid extension"""
-        probable_image_link = True
+        if probable_image_link:
+            link_image = event.text
 
-    if probable_image_link and "googleusercontent" in event_text_lower:
-        """reject links posted by google to prevent endless attachment loop"""
-        logger.debug("rejected link {} with googleusercontent".format(event.text))
-        return
+            if "imgur.com" in link_image:
+                """special imgur link handling"""
+                if not link_image.endswith((".jpg", ".gif", "gifv", "webm", "png")):
+                    link_image = link_image + ".gif"
+                link_image = "https://i.imgur.com/" + os.path.basename(link_image)
 
-    if probable_image_link:
-        link_image = event.text
+            link_image = link_image.replace(".webm",".gif")
+            link_image = link_image.replace(".gifv",".gif")
 
-        if "imgur.com" in link_image:
-            """special imgur link handling"""
-            if not link_image.endswith((".jpg", ".gif", "gifv", "webm", "png")):
-                link_image = link_image + ".gif"
-            link_image = "https://i.imgur.com/" + os.path.basename(link_image)
+            logger.info("getting {}".format(link_image))
 
-        link_image = link_image.replace(".webm",".gif")
-        link_image = link_image.replace(".gifv",".gif")
+            filename = os.path.basename(link_image)
+            r = yield from aiohttp.request('get', link_image)
+            raw = yield from r.read()
+            image_data = io.BytesIO(raw)
 
-        logger.info("getting {}".format(link_image))
+            image_id = yield from bot._client.upload_image(image_data, filename=filename)
 
-        filename = os.path.basename(link_image)
-        r = yield from aiohttp.request('get', link_image)
-        raw = yield from r.read()
-        image_data = io.BytesIO(raw)
-
-        image_id = yield from bot._client.upload_image(image_data, filename=filename)
-
-        yield from bot.coro_send_message(event.conv.id_, None, image_id=image_id)
-    if 'http' in event.text.lower()  and not probable_image_link:
-        words = event.text.split()
+            yield from bot.coro_send_message(event.conv.id_, None, image_id=image_id)
+        words  = str(event.text).split()
         for i in range(len(words)):
             if 'http' in words[i]:
-                url = words[i]
-        title = get_title(url)
-        if not title == 'Unable to open url :(':
-            link = shorten(url)
-            msg = _("** {} ** -- {}").format(title, link)
-        else:
-            msg = _("{}").format(title)
-        yield from bot.coro_send_message(event.conv, msg)
+                parsedurl = str(words[i])
+                not_image_but_link = True
+                break
+            else:
+                not_image_but_link = False
+        if not_image_but_link and not probable_image_link:
+            title = get_title(parsedurl)
+            if not title == 'Unable to open url :(':
+                link = shorten(parsedurl)
+                msg = _("** {} ** -- {}").format(title, link)
+            else:
+                msg = _("{}").format(title)
+            yield from bot.coro_send_message(event.conv, msg)
+        except BaseException as e:
+            simple = _('An Error Occurred')
+            msg = _('{} -- {}').format(str(e), event.text)
+            yield from bot.coro_send_message(CONTROL, msg)
+            yield from bot.coro_send_message(event.conv, simple)
