6c3460dc824fcec935b752dac5ce86b67276fabe
2019okulkarn <2019okulkarn@tjhsst.edu>
2016-01-23 14:52:46 -0500

    get last commit

--- hangupsbot/plugins/issue.py
+++ hangupsbot/plugins/issue.py
@@ -6,7 +6,28 @@ from links import *
 from control import *
 
 def _initialise():
-    plugins.register_admin_command(['issue'])
+    plugins.register_admin_command(['issue', 'commit'])
+
+def commit(bot, event, *args):
+    '''Get the latest commit'''
+    try:
+        url = 'https://api.github.com/repos/{}/{}/git/refs/heads/master'.format(REPO_OWNER, REPO_NAME)
+        get = requests.get(url)
+        data = json.loads(get.text)
+        commiturl = data[u'object'][u'url']
+        getcommit = requests.get(commiturl)
+        commitdata = json.loads(getcommit.text)
+        link = shorten(str(commitdata[u'html_url']))
+        committer = str(commitdata[u'committer'][u'name'])
+        date = str(commitdata[u'committer'][u'date'])
+        message = str(commitdata[u'message'])
+        msg = _('The last commit was {} by {} at {}<br>{}').format(message, committer, date, link)
+        yield from bot.coro_send_message(event.conv, msg)
+    except BaseException as e:
+        msg = _('{} -- {}').format(e, event.text)
+        simple = _('An Error Occurred. Please Try Again Later')
+        yield from bot.coro_send_message(event.conv, simple)
+        yield from bot.coro_send_message(CONTROL, msg)
 
 def issue(bot, event, *args):
     '''Create an issue on github.com using the given parameters.'''
