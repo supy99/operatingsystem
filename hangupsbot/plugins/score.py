42e93af439b92b25fcb0f0996af8232051e1d669
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-01-24 20:38:23 +0000

    add ! score. Fixes #42

---
+++ hangupsbot/plugins/score.py
@@ -0,0 +1,31 @@
+from espn import *
+import plugins
+from control import *
+
+def _initialise():
+    plugins.register_user_command(['score'])
+
+def score(bot, event, *args):
+    try:
+        list_ = ' '.join(args).split(' - ')
+        sport = list_[0]
+        team = list_[1]
+        scores = get_scores(sport.lower(), team.lower())
+        first = str(scores).split(':')
+        id = str(first[0])
+        id = id[2:-1]
+        scoreslist = scores[id]
+        team1 = scoreslist[0]
+        team1score = scoreslist[1]
+        team2 = scoreslist[2]
+        team2score = scoreslist[3]
+        msg = _('<b>{}:</b> {}<br><b>{}:</b> {}').format(team1, team1score, team2, team2score)
+        yield from bot.coro_send_message(event.conv, msg)
+    except BaseException as e:
+        simple = _('Unable to find scores')
+        msg = _(str(e))
+        yield from bot.coro_send_message(event.conv, simple)
+        yield from bot.coro_send_message(CONTROL, msg)
+	
+	    
+
