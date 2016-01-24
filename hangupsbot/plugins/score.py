1c1499018e5d0c69cc0bd9689a88546f8281644f
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-01-24 20:49:37 +0000

    show time

--- hangupsbot/plugins/score.py
+++ hangupsbot/plugins/score.py
@@ -19,7 +19,8 @@ def score(bot, event, *args):
         team1score = scoreslist[1]
         team2 = scoreslist[2]
         team2score = scoreslist[3]
-        msg = _('<b>{}:</b> {}<br><b>{}:</b> {}').format(team1, team1score, team2, team2score)
+        time = scoreslist[4]
+	msg = _('<b>{}:</b> {}<br><b>{}:</b> {}<br>{} - MAY NOT BE ACCURATE DURING GAMES').format(team1, team1score, team2, team2score, time)
         yield from bot.coro_send_message(event.conv, msg)
     except BaseException as e:
         simple = _('Unable to find scores')
