9aab75786cb735c5278f68b059a7458ef7d05c2b
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-01-25 15:13:12 -0500

    show message

--- hangupsbot/plugins/restart.py
+++ hangupsbot/plugins/restart.py
@@ -7,6 +7,7 @@ def _initialise():
     plugins.register_admin_command(['restart'])
 
 def restart(bot, event, *args):
-    os.execl(sys.executable, sys.executable, *sys.argv)
     msg = _('Bot has been restarted')
     yield from bot.coro_send_message(CONTROL, msg)
+    os.execl(sys.executable, sys.executable, *sys.argv)
+ 
