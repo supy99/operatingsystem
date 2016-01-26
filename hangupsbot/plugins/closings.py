db60185125bcfa46207821cf8cd026e4b6d673b4
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-01-26 15:51:20 -0500

    simplification

--- hangupsbot/plugins/closings.py
+++ hangupsbot/plugins/closings.py
@@ -2,7 +2,6 @@ import plugins
 import json
 from requests import get
 from control import *
-from bs4 import BeautifulSoup
 import asyncio
 
 def _initialise():
@@ -11,65 +10,42 @@ def _initialise():
 
 
 def checklcps():    
-    try:
-        r = get('http://www.nbcwashington.com/weather/school-closings/')
-        html = r.text
-        soup = BeautifulSoup(html, 'html.parser')
-        schools = []
-        for school in soup.find_all('p'):
-            schools.append(school.text)
-
-        for i in range(len(schools)):
-            if 'Loudoun County' in schools[i]:
-                check = str(schools[i])
-        status = check.replace('Loudoun County Schools', '')
-        msg = _('LCPS is {}').format(status)
-        return {'message': msg,
-                'simple': ''}
-    except BaseException as e:
-        simple = _('An Error Occurred')
-        msg = _('{} -- {}').format(str(e), event.text)
-        return {'message': msg,
-                'simple': simple}
+    page = get('http://wogloms.com/closings/status.php')
+    data = json.loads(page.text)
+    status = data['Loudoun']
+    return status
     
 
 def checkfcps():
-    try:
-        page = get('https://ion.tjhsst.edu/api/emerg?format=json')
-        data = json.loads(page.text)
-        status = data['status']
-        if status:
-            message = data['message']
-            message = message.replace('<p>', '')
-            message = message.replace('</p>', '')
-            msg = _(message)
-        else:
-            msg = _('FCPS is open')
-        return {'message':msg,
-                'simple': ''}
-    except BaseException as e:
-        simple = _('An Error Occurred')
-        msg = _('{} -- {}').format(str(e), event.text)
-        return {'message':msg,
-                'simple': simple}
+    page = get('http://wogloms.com/closings/status.php')
+    data = json.loads(page.text)
+    status = data['Fairfax']
+    return status
+
 @asyncio.coroutine
 def _fcps(bot, event, command):
-    tbc = ['fcps', 'fairfax', 'ffx']
-    if any(term in event.text.lower() for term in tbc):
-        check = checkfcps()
-        if check['simple'] == '':
-            yield from bot.coro_send_message(event.conv, check['message'])
-        else:
-            yield from bot.coro_send_message(event.conv, check['simple'])
-            yield from bot.coro_send_message(CONTROL, check['message'])
+    try:
+        tbc = ['fcps', 'fairfax', 'ffx']
+        if any(term in event.text.lower() for term in tbc):
+            check = checkfcps()
+            msg = _('FCPS Is Currently <b>{}<b>').format(check.upper())
+            yield from bot.coro_send_message(event.conv, msg)
+    except BaseException as e:
+        simple = _('An Error Occurred')
+        msg = _('{} -- {}').format(str(e), event.text)    
+        yield from bot.coro_send_message(event.conv, simple)
+        yield from bot.coro_send_message(CONTROL, message)
 
 @asyncio.coroutine
 def _lcps(bot, event, command):
-    tbc = ['lcps', 'loudoun']
-    if any(term in event.text.lower() for term in tbc):
-        check = checklcps()
-        if check['simple'] == '':
-            yield from bot.coro_send_message(event.conv, check['message'])
-        else:
-            yield from bot.coro_send_message(event.conv, check['simple'])
-            yield from bot.coro_send_message(CONTROL, check['message'])
+    try:
+        tbc = ['lcps', 'loudoun']
+        if any(term in event.text.lower() for term in tbc):
+            check = checklcps()
+            msg = _('LCPS Is Currently <b>{}<b>').format(check.upper())
+            yield from bot.coro_send_message(event.conv, msg)
+    except BaseException as e:
+        simple = _('An Error Occurred')
+        msg = _('{} -- {}').format(str(e), event.text)    
+        yield from bot.coro_send_message(event.conv, simple)
+        yield from bot.coro_send_message(CONTROL, message)
