8f557e0df60d8f761c0f0359fff5ce61b014301d
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-01-26 15:59:35 -0500

    add fcps as a command

--- hangupsbot/plugins/closings.py
+++ hangupsbot/plugins/closings.py
@@ -5,6 +5,7 @@ from control import *
 import asyncio
 
 def _initialise():
+    plugins.register_user_command(['fcps'])
     plugins.register_handler(_fcps, type="message")
     plugins.register_handler(_lcps, type="message")
 
@@ -21,6 +22,26 @@ def checkfcps():
     data = json.loads(page.text)
     status = data['Fairfax']
     return status
+
+def fcps(bot, event, *args):
+'''This command checks for closings in the Fairfax County Public Schools Area. Data taken from TJHSST.'''
+    try:
+        page = get('https://ion.tjhsst.edu/api/emerg?format=json')
+        data = json.loads(page.text)
+        status = data['status']
+        if status:
+            message = data['message']
+            message = message.replace('<p>', '')
+            message = message.replace('</p>', '')
+            msg = _(message)
+        else:
+            msg = _('FCPS is open')
+        yield from bot.coro_send_message(event.conv, msg)
+    except BaseException as e:
+        simple = _('An Error Occurred')
+        msg = _('{} -- {}').format(str(e), event.text)
+        yield from bot.coro_send_message(event.conv, simple)
+        yield from bot.coro_send_message(CONTROL, msg)
 
 @asyncio.coroutine
 def _fcps(bot, event, command):
