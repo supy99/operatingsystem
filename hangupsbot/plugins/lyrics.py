64c07c8e5f262d3392f31dc9efa8ef2d295eaa3d
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-01-27 16:59:42 +0000

    revert to a to z lyrics

--- hangupsbot/plugins/lyrics.py
+++ hangupsbot/plugins/lyrics.py
@@ -1,27 +1,29 @@
-import plugins, re
+from urllib.request import urlopen as get
+from bs4 import BeautifulSoup as soup
+import plugins
 from control import *
-from requests import get
-from bs4 import BeautifulSoup
-from apikeys import lyricsnmusic as key
+
+def getlyrics(title, artist):
+	url = 'http://www.azlyrics.com/lyrics/' + artist + '/' + title + '.html'
+	r = get(url)
+	html = r.read()
+	nice = soup(html, 'html.parser')
+	text = nice.get_text()
+	newartist = artist.upper()
+	tosplit = newartist + " LYRICS"
+	lyr = text.split(tosplit)
+	lyrics = lyr[2]
+	lyrics_ = lyrics.split('Submit Corrections')
+	final = lyrics_[0]
+	return final
 
 def _initialise():
-    plugins.register_user_command(["lyrics"])
+	plugins.register_admin_command(['testlyrics'])
 
-def lyrics(bot, event, *args):
-    '''get lyrics from lyricsnmusic.com. THIS IS NOT PERFECT EVERY TIME. Format is /bot lyrics <song>'''
-    try:
-        payload = {'apikey': key, 'q': str(args)}
-        r = get('http://api.lyricsnmusic.com/songs', params=payload)
-        info = r.json()[0]
-        lyrics = BeautifulSoup(get(info['url']).text, 'html.parser')
-        msg = [None, None] 
-        msg[0] = _('<b>Title: </b>{}, <b>Artist: </b>{}').format(
-            info['title'], info['artist']['name'])
-        msg[1] = _(lyrics.pre.string)
-        yield from bot.coro_send_message(event.conv, msg[0])
-        yield from bot.coro_send_message(event.conv, msg[1])
-    except BaseException as e:
-        simple = _("Lyrics not found")
-        msg = _('{} -- {}').format(str(e), event.text)
-        yield from bot.coro_send_message(event.conv, simple)
-        yield from bot.coro_send_message(CONTROL,msg)
+def testlyrics(bot, event, *args):
+	message = ' '.join(args)
+	title = message.split(' by ')[0]
+	artist = message.split(' by ')[1]
+	g = getlyrics(title, artist)
+	msg = _(g)
+	yield from bot.coro_send_message(CONTROL, msg)
