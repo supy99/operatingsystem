6d80ff3c088c2ffdfbef1e0ac2be4dd52a568bb9
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-01-26 19:06:51 -0500

    for testing

--- hangupsbot/plugins/poll.py
+++ hangupsbot/plugins/poll.py
@@ -33,7 +33,7 @@ def vote(bot, event, *args):
             vote = spl[0]
             poll = spl[1]
             path = bot.memory.get_by_path(['polls', poll])
-            path[event.user.id_.chat_id] = vote
+            path[event.user.first_name] = vote
             bot.memory.set_by_path(['polls', poll], path)
             bot.memory.save()
             msg = _('Your vote for {} has been recorded as {}').format(poll, vote)
@@ -47,34 +47,30 @@ def vote(bot, event, *args):
         yield from bot.coro_send_message(CONTROL, msg)
 
 def results(bot, event, *args):
-    try:
-        poll = ' '.join(args)
-        users = []
-        votes = []
-        names = []
-        msg = []
-        path = bot.memory.get_by_path(["polls", poll])
-        for person in path:
-            spl = person.split(':')
-            userid = spl[0]
-            vote = spl[1]
-            users.append(userid)
-            votes.append(vote)
-        [str(id_) for id_ in users]
-        [str(vote) for vote in votes]
-        for user in users:
-            name = bot.user_memory_get(user, 'first_name')
-            names.append(name)
-        for i in range(len(names)):
-            result = '{} voted {}<br>'.format(names[i], votes[i])
-            msg.append(result)
-        final = ''.join(msg)
-        yield from bot.coro_send_message(event.conv, final)
-    except BaseException as e:
+'''    try:'''
+    poll = ' '.join(args)
+    votes = []
+    names = []
+    msg = []
+    path = bot.memory.get_by_path(["polls", poll])
+    for person in path:
+        spl = person.split(':')
+        name = spl[0]
+        vote = spl[1]
+        names.append(name)
+        votes.append(vote)
+    [str(name) for name in names]
+    [str(vote) for vote in votes]
+    for i in range(len(names)):
+        result = '{} voted {}<br>'.format(names[i], votes[i])
+        msg.append(result)
+    final = ''.join(msg)
+    yield from bot.coro_send_message(event.conv, final)
+'''    except BaseException as e:
         simple = _('Either an error occurred or there is no poll by that name')
         msg = _('{} -- {}').format(str(e), event.text)
         yield from bot.coro_send_message(event.conv, simple)
-        yield from bot.coro_send_message(CONTROL, msg)
+        yield from bot.coro_send_message(CONTROL, msg)'''
 
 
 
