2855160e237b7b0dd2359c8470efcdd3100e2163
Omkar Kulkarni <2019okulkarn@tjhsst.edu>
2016-01-31 14:50:22 -0500

    add google search

--- hangupsbot/plugins/g.py
+++ hangupsbot/plugins/g.py
@@ -1,11 +1,47 @@
 import plugins
-
+from apikeys import cx, google
 from links import shorten
 from control import *
+import json
 
 from urllib.parse import quote
 def _initialise():
-    plugins.register_user_command(['lmgtfy'])
+    plugins.register_user_command(['lmgtfy', 'google', 'g'])
+
+def search(term):
+    r = get('https://www.googleapis.com/customsearch/v1', params={'key': google, 'cx': cx, 'q': term})
+    data = json.loads(r.text)
+    link = shorten(data['items'][0]['link'])
+    return link
+
+def google(bot, event, *args):
+    try:
+        if args:
+            s = search(' '.join(args))
+            msg = _('I searched Google and got {}').format(s)
+        else:
+            msg = _('What should I ask Google to answer?')
+        yield from bot.coro_send_message(event.conv, msg)
+    except:
+        tb = traceback.format_exc()
+        msg = _('{} -- {}').format(tb, event.text)
+        yield from bot.coro_send_message(CONTROL, msg)
+        yield from bot.coro_send_message(event.conv, _('An Error Occured'))
+
+
+def g(bot, event, *args):
+    try:
+        if args:
+            s = search(' '.join(args))
+            msg = _('I searched Google and got {}').format(s)
+        else:
+            msg = _('What should I ask Google to answer?')
+        yield from bot.coro_send_message(event.conv, msg)
+    except:
+        tb = traceback.format_exc()
+        msg = _('{} -- {}').format(tb, event.text)
+        yield from bot.coro_send_message(CONTROL, msg)
+        yield from bot.coro_send_message(event.conv, _('An Error Occured'))
 
 def lmgtfy(bot, event, *args):
     '''Returns an lmgtfy link from http://lmgtfy.com/ Format is /bot lmgtfy <what to google>'''
